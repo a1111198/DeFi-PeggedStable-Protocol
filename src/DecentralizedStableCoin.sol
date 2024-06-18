a535638b8fbf4705482ebc6b0939accf34e7bf5d
aa1111198 <akbansal123456@gmail.com>
2024-06-18 13:44:43 +0530

    feat:DSC Implmeneted

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity 0.8.19;
+import {ERC20Burnable, ERC20} from "@openzepplin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzepplin/contracts/access/Ownable.sol";
+
+/*
+* @title DecentralizedStableCoin
+* @author Akash Bansal
+* colletral: Exogenous (ETH & BTC )
+* Minting: Algorthmic 
+* Relative Stability: Pegged to USDC
+*
+* This is the Contract meant to be governed by DSCEngine. This contract is just the ERC20
+Implementation of our stableCoin system.
+*/
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin_MustBeMoreThanZero();
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+    error DecentralizedStableCoin_MustBeNonZeroAddress();
+
+    constructor() ERC20("DescentralizedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        if (_amount > balance) {
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin_MustBeNonZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
